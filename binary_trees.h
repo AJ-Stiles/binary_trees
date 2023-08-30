b2726abecc13c219f67ebd99e16daf587efbaf35
Jude Alistair <jude55593@gmail.com>
2023-08-30 10:15:26 -0700

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -45,21 +45,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t binary_tree_depth(const binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-int binary_tree_is_complete(const binary_tree_t *tree);
-int is_complete_recursive(const binary_tree_t *tree, int index,
-		int nodes_count);
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-int binary_tree_is_bst(const binary_tree_t *tree);
-int is_bst_recursive(const binary_tree_t *tree, const int *min,
-		const int *max);
-bst_t *bst_insert(bst_t **tree, int value);
-bst_t *array_to_bst(int *array, size_t size);
-bst_t *bst_search(const bst_t *tree, int value);
-bst_t *bst_remove(bst_t *root, int value);
-bst_t *bst_min_value_node(bst_t *node);
-int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
