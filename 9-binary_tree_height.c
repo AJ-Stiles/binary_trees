a1416001b7f80f4ed2df0b665d5fb380eebe5ea0
Jude Alistair <jude55593@gmail.com>
2023-08-31 00:39:12 -0700

    upd

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
 	size_t height_left = binary_tree_height(tree->left);
 	size_t height_right = binary_tree_height(tree->right);
+
+	if (tree == NULL)
+		return (0);
 
 	return (1 + (height_left > height_right ? height_left : height_right));
 }
