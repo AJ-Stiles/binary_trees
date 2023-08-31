cc06a96be871e3f4f697f49dc6adedf46ee76741
Jude Alistair <jude55593@gmail.com>
2023-08-31 03:24:35 -0700

    task 16 update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,3 +37,43 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (left_height == right_height);
 }
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: Pointer to the node to measure the depth
+ *
+ * Return: Depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
+/**
+ * is_perfect_recursive - Checks if a binary tree is perfect recursively
+ * @tree: Pointer to the root node of the tree
+ * @depth: Depth of the current node
+ * @level: Current level of the tree
+ *
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
+int is_perfect_recursive(const binary_tree_t *tree, size_t depth, size_t level)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (depth == level + 1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	return (is_perfect_recursive(tree->left, depth, level + 1) &&
+			is_perfect_recursive(tree->right, depth, level + 1));
+}
