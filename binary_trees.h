cba7850100f75bab561cf24b2b1b712e0d391718
Jude Alistair <jude55593@gmail.com>
2023-08-31 00:00:25 -0700

    binary

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,15 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <limits.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,6 +27,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* Function Prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
